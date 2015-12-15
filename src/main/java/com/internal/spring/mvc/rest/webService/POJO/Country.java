d0855e67af10aac8f51f0d04faca5a58b2bba168
Raktotpal Bordoloi <rpal.4m.assam@gmail.com>
2015-12-15 16:28:15 +0530

    MyGIT | BackUP | Spring / REST / MVC / WebService | 14122015

---
+++ src/main/java/com/internal/spring/mvc/rest/webService/POJO/Country.java
@@ -0,0 +1,34 @@
+package com.internal.spring.mvc.rest.webService.POJO;
+
+public class Country {
+
+	private String label;
+	private String dimensionName;
+	
+	public Country() {
+		
+	}
+
+	public Country(String label, String dimensionName) {
+		super();
+		this.setLabel(label);
+		this.setDimensionName(dimensionName);
+	}
+
+	public String getDimensionName() {
+		return dimensionName;
+	}
+
+	public void setDimensionName(String dimensionName) {
+		this.dimensionName = dimensionName;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+}
---
+++ src/main/java/com/internal/spring/mvc/rest/webService/controller/CountryController.java
@@ -0,0 +1,44 @@
+package com.internal.spring.mvc.rest.webService.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.internal.spring.mvc.rest.webService.POJO.Country;
+
+@RestController
+/* @RequestMapping("/hello") */
+public class CountryController {
+	@RequestMapping(value = "/countries", method = RequestMethod.GET, headers = "Accept=application/json")
+	public List<Country> getCountries() {
+		List<Country> listOfCountries = new ArrayList<Country>();
+		listOfCountries = createCountryList();
+		return listOfCountries;
+	}
+	
+	@RequestMapping(value = "/addStudent", method = RequestMethod.POST)
+	   public @ResponseBody List<Country> addStudent(@RequestBody List<Country> cnts) {
+	      
+	      return cnts;
+	   }
+
+	// Utiliy method to create country list.
+	public List<Country> createCountryList() {
+		Country indiaCountry = new Country("1", "India");
+		Country bhutanCountry = new Country("2", "Bhutan");
+		Country chinaCountry = new Country("4", "China");
+		Country nepalCountry = new Country("3", "Nepal");
+
+		List<Country> listOfCountries = new ArrayList<Country>();
+		listOfCountries.add(indiaCountry);
+		listOfCountries.add(chinaCountry);
+		listOfCountries.add(nepalCountry);
+		listOfCountries.add(bhutanCountry);
+		return listOfCountries;
+	}
+}
