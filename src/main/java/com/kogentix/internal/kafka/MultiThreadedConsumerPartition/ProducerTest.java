88d5467439fab171f3472f09cdeab477b442cd7a
Raktotpal Bordoloi <rpal.4m.assam@gmail.com>
2017-06-09 15:47:17 +0530

    Added multi-threaded consumer with multi-partitioned topic.
    This package has correct implementation - each threaded consumer is responsible for one partition of the topic.

---
+++ src/main/java/com/kogentix/internal/kafka/MultiThreadedConsumerPartition/ProducerTest.java
@@ -0,0 +1,35 @@
+package com.kogentix.internal.kafka.MultiThreadedConsumerPartition;
+
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+//import kafka.producer.Producer;
+//import kafka.producer.KeyedMessage;
+//import kafka.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class ProducerTest {
+
+  public static void main(String[] args) throws InterruptedException {
+    Properties props = new Properties();
+    props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+    props.put(ProducerConfig.ACKS_CONFIG, "1");
+
+    KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+    // String msg = (String) args[0];
+    for (int i = 0;; i++) {
+      System.out.println("in loop");
+      String msg = "Trying Producer in" + i + System.currentTimeMillis();
+      producer.send(new ProducerRecord<String, String>("rpal-multi-part", msg));
+      TimeUnit.SECONDS.sleep(1);
+    }
+    // producer.close();
+
+  }
+
+}
